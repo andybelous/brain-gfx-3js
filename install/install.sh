fb5ef63682bc9f919ec5880684fd38761c852f5a
Reuben Kraft <reuben.kraft@gmail.com>
2021-01-28 11:20:20 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
- nvm install node
- sudo yum install atk java-atk-wrapper at-spi2-atk gtk3 libXt
- 
+bash ~/.nvm/nvm.sh
+nvm install node
+sudo yum install atk java-atk-wrapper at-spi2-atk gtk3 libXt -y
+npm i puppeteer
+npm i puppeteer-core
