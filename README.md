10ee91a7f5341005381c6d128bb369d723d8f55d
Reuben Kraft <reuben.kraft@gmail.com>
2021-01-28 10:12:54 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # brain-gfx-3js
+
+## install
+
+bash install.sh
+
+## run
+
+node example.js
+
+Should create a example.png
---
+++ install/install.sh
@@ -0,0 +1,5 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+ nvm install node
+ sudo yum install atk java-atk-wrapper at-spi2-atk gtk3 libXt
+ 
